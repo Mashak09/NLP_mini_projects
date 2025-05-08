ac9e9da032d4b32d73b2119057b0e75e5fba1d7e
Mashak Ibrahim <69128375+Mashak09@users.noreply.github.com>
2025-05-08 14:35:51 +0200

    Update NLP_Day05.py

--- NLP_Day05.py
+++ NLP_Day05.py
@@ -1,11 +1,15 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 #skcitlearn-ml library,feature_extracation: converts text to numbers and tfid vectorizer tf-idf to convert text into numbers
+
 documents=['DON is back','Is DON really Back', 'Yes DON is really back']
+
 tfid_vectorizer=TfidfVectorizer()
 #intitializing TfidfVectorizer
+
 tfidf_matrix= tfid_vectorizer.fit_transform(documents)
+#converts the text into matrix form using the tranform 
 
 print('Vocabulary: ',tfid_vectorizer.vocabulary_)
 
 print('tfidf_matrix.\n',tfidf_matrix.toarray())
-
+#convert the matrix into an array an array of numbers bcz its easier to read
